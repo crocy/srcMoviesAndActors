a9efa5837bc46ed69dd2c358ff7a7c1679a4660e
Nejc Krokter <teh.croc@gmail.com>
2024-06-02 10:24:35 +0200

    Initial commit.

---
+++ actors/src/main/java/com/src/actors/ActorsApplication.java
@@ -0,0 +1,13 @@
+package com.src.actors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorsApplication.class, args);
+	}
+
+}
